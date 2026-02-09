e9d3320bbc7ac26903749aead5dcc3fa0049fcc8
Chrovo <chrovo1@gmail.com>
2026-02-09 12:04:46 -0500

    Fix text covered by squares

--- systems.fyi/src/Square.tsx
+++ systems.fyi/src/Square.tsx
@@ -9,7 +9,7 @@ const Box = ({date}: BoxProps) => {
     return (
         <div className="group relative inline-block">
             <div className={`${color} w-4 h-4 rounded-xs border border-gray-400`} onClick={() => setColor("bg-green-400")} />
-            <div className="absolute hidden group-hover:block bg-white border border-gray-300 rounded-md p-2">
+            <div className="absolute hidden group-hover:block bg-white border border-gray-300 rounded-md p-2 z-366">
                 {date.getMonth()}/{date.getDate()}/{date.getFullYear()}
             </div>
         </div>
